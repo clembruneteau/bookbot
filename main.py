ed78741b111b71beb0857a3131b89e9081835fa6
Clément Bruneteau <cbruneteau@einden.fr>
2025-04-10 23:24:50 +0200

    refactor main function

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
-def main():
-    print(get_book_text("books/frankenstein.txt"))
 
 main()
