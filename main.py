a10d0960a8c4b91c5e8702b8f6558bab9566a488
Clément Bruneteau <cbruneteau@einden.fr>
2025-04-10 23:39:08 +0200

    refactoring: move stat function in module stats

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = number_of_words(text)
+    num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
-def number_of_words(text):
-    words = text.split()
-    return len(words)
 
 main()
