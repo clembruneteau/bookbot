f7fcf512ec98d28f5b3cf0a6f40765bdf84f8827
Clément Bruneteau <cbruneteau@einden.fr>
2025-04-11 09:36:49 +0200

    add 2 functions to print report and usage + manage sys.argv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_num_words, 
     get_num_chars, 
@@ -5,24 +6,35 @@ from stats import (
 )
 
 def main():
-    book_path = "books/frankenstein.txt"
-
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
+    if len(sys.argv) < 2:
+        print_usage()
+    
+    book_path = sys.argv[1]
 
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    
     num_chars = get_num_chars(text)
     sorted_chars = sort_chars_dict(num_chars)
 
+    print_report(book_path, num_words, sorted_chars)
+
+
+def print_usage():
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+
+def print_report(book_path, num_words, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
     print("--------- Character Count -------")
     for item in sorted_chars:
-        if item["char"].isalpha():
-            print(item["char"] + ": " + str(item["num"]))
+        if item['char'].isalpha():
+            print(item['char'] + ": " + str(item['num']))
 
     print("============= END ===============")
 
@@ -30,5 +42,6 @@ def main():
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
+
 
 main()
